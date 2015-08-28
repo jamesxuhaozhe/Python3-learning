76491626265267480385adff1549c0c3588b8b7c
Haozhe Xu <jamesxuhaozhe@gmail.com>
2015-08-28 14:54:43 -0700

    first commit

---
+++ ex2.py
@@ -0,0 +1,2 @@
+print('This is a test', 'dont be serious', 'chill out')
+print('100 + 200 = ', 300)
---
+++ ex4.py
@@ -0,0 +1,3 @@
+name = input('Please tell us what is your name?')
+print('Hello', name, 'What can I do for you?')
+
